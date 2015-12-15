12322e0501d946440932127d93714dcd6b2abcc5
Itay Sagui <saguiitay@hotmail.com>
2015-12-16 01:52:27 +0200

    Improved BinarySearchTree: - Insert is no longer recursive (original kept for reference) - Add count to tree - Added additional enumerators

--- src/DataStructure.Tests/Program.cs
+++ src/DataStructure.Tests/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DataStructures;
 using DataStructures.Trees;
 
 namespace DataStructure.Tests
@@ -9,7 +10,7 @@ namespace DataStructure.Tests
         {
             MainTree();
 
-            MainSetAll();
+            //MainSetAll();
         }
 
 
@@ -42,29 +43,60 @@ namespace DataStructure.Tests
 
         static void MainTree()
         {
-            var bst = new BinarySearchTree<int>();
-            bst.Insert(5);
-            bst.Insert(1);
-            bst.Insert(8);
-            bst.Insert(2);
-            bst.Insert(6);
-            bst.Insert(9);
+            //var bst = new BinarySearchTree<int>();
+            //bst.Insert(5);
+            //bst.Insert(1);
+            //bst.Insert(8);
+            //bst.Insert(2);
+            //bst.Insert(7);
+            //bst.Insert(9);
+            //bst.Insert(13);
+            //bst.Insert(11);
+            //bst.Insert(6);
+            //bst.Insert(14);
+
+            var bst = BinaBinarySearchTreeExtensions.FromString("5 1 2 8 6 7 9 13 11 14");
 
             //        5
             //      /  \
-            //     1    8
-            //    / \  / \
-            //      2  6  9
+            //     1     8
+            //    / \   / \
+            //      2  6   9
+            //          \   \
+            //           7  13
+            //             /  \
+            //            11   14
 
-            // Depth PreOrder: 5 1 2 8 6 9
 
-            for (int i = 0; i < 12; i++)
+
+            for (int i = 0; i < 15; i++)
                 Console.WriteLine("Contains {0}: {1}", i, bst.Contains(i));
 
+            Console.WriteLine();
+
+            // Depth PreOrder: 5 1 2 8 6 7 9 13 11 14
             Console.WriteLine("PreOrder: ");
             var preOrder = bst.GetPreOrderEnumerator();
             while (preOrder.MoveNext())
                 Console.Write(" {0} ", preOrder.Current.Value);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // Depth InOrder: 1 2 5 6 7 8 9 11 13 14
+            Console.WriteLine("InOrder: ");
+            var inOrder = bst.GetInOrderEnumerator();
+            while (inOrder.MoveNext())
+                Console.Write(" {0} ", inOrder.Current.Value);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // Depth LevelOrder: 5 1 8 2 6 9 7 13 11 14
+            Console.WriteLine("LevelOrder: ");
+            var levelOrder = bst.GetLevelOrderEnumerator();
+            while (levelOrder.MoveNext())
+                Console.Write(" {0} ", levelOrder.Current.Value);
 
         }
     }
--- src/DataStructure/Trees/BinarySearchTree.cs
+++ src/DataStructure/Trees/BinarySearchTree.cs
@@ -1,17 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DataStructures.Trees
 {
     public class BinarySearchTree<T> where T : IComparable
     {
         private Node<T> _root;
+        private int _count;
 
-        public virtual void Insert(T value)
-        {
-            InsertInternal(value, ref _root);
-        }
+        public int Count => _count;
 
         public bool Contains(T value)
         {
@@ -23,14 +22,44 @@ namespace DataStructures.Trees
             return false;
         }
 
+
+        public virtual void InsertRecursive(T value)
+        {
+            InsertInternal(value, ref _root);
+        }
+
+        
+        public void Insert(T value)
+        {
+            Node<T> parentNode = null;
+            for (Node<T> node = _root; node != null; parentNode = node, node = value.CompareTo(node.Value) >= 0 ? node.Right : node.Left)
+            {
+                if (node.Value.Equals(value))
+                    return;
+            }
+            var newNode = new Node<T> {Value = value, Parent = parentNode};
+            if (parentNode != null)
+            {
+                if (value.CompareTo(parentNode.Value) >= 0)
+                    parentNode.Right = newNode;
+                else
+                    parentNode.Left = newNode;
+            }
+            else
+                _root = newNode;
+        }
+
         private void InsertInternal(T value, ref Node<T> node, Node<T> parent = null)
         {
             if (node == null)
-                node = new Node<T>()
+            {
+                _count++;
+                node = new Node<T>
                     {
                         Value = value,
                         Parent = parent
                     };
+            }
             else if (value.CompareTo(node.Value) < 0)
             {
                 Node<T> left = node.Left;
@@ -54,12 +83,22 @@ namespace DataStructures.Trees
             return new PreOrderEnumerator(this);
         }
 
-        public class PreOrderEnumerator : IEnumerator<Node<T>>
+        public IEnumerator<Node<T>> GetInOrderEnumerator()
+        {
+            return new InOrderEnumerator(this);
+        }
+
+        public IEnumerator<Node<T>> GetLevelOrderEnumerator()
+        {
+            return new LevelOrderEnumerator(this);
+        }
+
+        private class PreOrderEnumerator : IEnumerator<Node<T>>
         {
             private readonly BinarySearchTree<T> _tree;
-            private Stack<Node<T>> _queue;
+            private Stack<Node<T>> _stack;
 
-            public Node<T> Current => _queue.Peek();
+            public Node<T> Current => _stack.Peek();
 
             object IEnumerator.Current => Current;
 
@@ -74,19 +113,115 @@ namespace DataStructures.Trees
 
             public bool MoveNext()
             {
-                if (_queue == null)
+                if (_stack == null)
                 {
-                    _queue = new Stack<Node<T>>();
+                    _stack = new Stack<Node<T>>();
                     if (_tree?._root != null)
-                        _queue.Push(_tree._root);
+                        _stack.Push(_tree._root);
                 }
                 else
                 {
-                    Node<T> node = _queue.Pop();
+                    Node<T> node = _stack.Pop();
                     if (node.Right != null)
-                        _queue.Push(node.Right);
+                        _stack.Push(node.Right);
                     if (node.Left != null)
-                        _queue.Push(node.Left);
+                        _stack.Push(node.Left);
+                }
+                return _stack.Count > 0;
+            }
+
+            public void Reset()
+            {
+                _stack = null;
+            }
+        }
+
+
+        private class InOrderEnumerator : IEnumerator<Node<T>>
+        {
+            private readonly BinarySearchTree<T> _tree;
+            private Stack<Node<T>> _stack;
+
+            public Node<T> Current => _stack.Peek();
+
+            object IEnumerator.Current => Current;
+
+            public InOrderEnumerator(BinarySearchTree<T> tree)
+            {
+                _tree = tree;
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                Node<T> node = null;
+                if (_stack == null)
+                {
+                    _stack = new Stack<Node<T>>();
+                    if (_tree?._root != null)
+                        node = _tree._root;
+                }
+                else
+                {
+                    node = _stack.Pop();
+                    node = node.Right;
+                }
+                if (node != null)
+                    PushLeft(node);
+                return _stack.Count > 0;
+            }
+
+            private void PushLeft(Node<T> node)
+            {
+                do
+                {
+                    _stack.Push(node);
+                    node = node.Left;
+                } while (node != null);
+            }
+
+            public void Reset()
+            {
+                _stack = null;
+            }
+        }
+
+        private class LevelOrderEnumerator : IEnumerator<Node<T>>
+        {
+            private readonly BinarySearchTree<T> _tree;
+            private Queue<Node<T>> _queue;
+
+            public Node<T> Current => _queue.Peek();
+
+            object IEnumerator.Current => Current;
+
+            public LevelOrderEnumerator(BinarySearchTree<T> tree)
+            {
+                _tree = tree;
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                if (_queue == null)
+                {
+                    _queue = new Queue<Node<T>>();
+                    if (_tree?._root != null)
+                        _queue.Enqueue(_tree._root);
+                }
+                else
+                {
+                    Node<T> node = _queue.Dequeue();
+                    if (node.Left != null)
+                        _queue.Enqueue(node.Left);
+                    if (node.Right != null)
+                        _queue.Enqueue(node.Right);
                 }
                 return _queue.Count > 0;
             }
@@ -95,6 +230,19 @@ namespace DataStructures.Trees
             {
                 _queue = null;
             }
+        }
+    }
+
+    public static class BinaBinarySearchTreeExtensions
+    {
+        public static BinarySearchTree<int> FromString(string data)
+        {
+            var bst = new BinarySearchTree<int>();
+            var values = data.Split(' ').Select(x => int.Parse(x));
+            foreach (var value in values)
+                bst.Insert(value);
+
+            return bst;
         }
     }
 }
